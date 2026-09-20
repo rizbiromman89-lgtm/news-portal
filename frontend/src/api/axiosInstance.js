1c1f359f835380f89ae7424f1e4fbb3dd04312d3
Romman <rizbiromman89@gmail.com>
2026-09-21 00:23:18 +0600

    Update API base URL for Render deployment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://news-portal-59mm.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
